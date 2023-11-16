460dec8efbc15a798f6e462d8145d28420047d18
Matt Ruwe <matthew.ruwe@kiewit.com>
2023-11-15 22:43:39 -0600

    initial commit

---
+++ PrizeWinner/Hubs/PrizeWinnerHub.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace PrizeWinner.Hubs
+{
+    public class PrizeWinnerHub : Hub
+    {
+        private static List<Session> _sessions = new List<Session>();
+
+        public async Task CreateSession()
+        {
+            var sessionId = Guid.NewGuid().ToString();
+            _sessions.Add(new Session
+            {
+                SessionId = sessionId
+            });
+            await Groups.AddToGroupAsync(Context.ConnectionId, sessionId);
+            await Clients.Caller.SendAsync("SessionStarted", sessionId);
+        }
+
+        public async Task JoinSession(string sessionId, string userName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, sessionId);
+            var user = new User() {ConnectionId = Context.ConnectionId, UserName = userName};
+            _sessions.Single(s => s.SessionId == sessionId)?.Users.Add(user);
+
+            await Clients.Group(sessionId).SendAsync("UserJoined", user);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            var session = _sessions.SingleOrDefault(s => s.Users.Any(u => u.ConnectionId == Context.ConnectionId));
+            if (session?.SessionId != null)
+            {
+                var user = session.Users.Single(u => u.ConnectionId == Context.ConnectionId);
+                session.Users.Remove(user);
+                await Clients.Group(session.SessionId).SendAsync("UserLeft", user);
+            }
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+
+    public class Session
+    {
+        public string? SessionId { get; set; }
+        public List<User> Users { get; set; } = new List<User>();
+    }
+
+    public class User
+    {
+        public string ConnectionId { get; set; }
+        public string UserName { get; set; }
+    }
+}
---
+++ PrizeWinner/Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.ResponseCompression;
+using PrizeWinner.Client.Pages;
+using PrizeWinner.Components;
+using PrizeWinner.Hubs;
+
+namespace PrizeWinner
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddSignalR();
+            builder.Services.AddResponseCompression(opts =>
+            {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                    new[] { "application/octet-stream" });
+            });
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents()
+                .AddInteractiveWebAssemblyComponents();
+
+            var app = builder.Build();
+            app.UseResponseCompression();
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveWebAssemblyRenderMode()
+                .AddInteractiveServerRenderMode()
+                .AddAdditionalAssemblies(typeof(Counter).Assembly);
+            app.MapHub<PrizeWinnerHub>("/prizewinnerhub");
+
+            app.Run();
+        }
+    }
+}
