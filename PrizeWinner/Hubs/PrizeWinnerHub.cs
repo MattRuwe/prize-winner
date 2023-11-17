68c4f7a790d5d50783541e712911fc232421e2cd
Matt Ruwe <matthew.ruwe@kiewit.com>
2023-11-16 21:48:39 -0600

    It's mostly working, but needs a lot of UI improvements

--- PrizeWinner/Hubs/PrizeWinnerHub.cs
+++ PrizeWinner/Hubs/PrizeWinnerHub.cs
@@ -11,7 +11,8 @@ namespace PrizeWinner.Hubs
             var sessionId = Guid.NewGuid().ToString();
             _sessions.Add(new Session
             {
-                SessionId = sessionId
+                SessionId = sessionId,
+                Users = new List<User>() { new User() { ConnectionId = Context.ConnectionId, IsHost = true } }
             });
             await Groups.AddToGroupAsync(Context.ConnectionId, sessionId);
             await Clients.Caller.SendAsync("SessionStarted", sessionId);
@@ -20,15 +21,32 @@ namespace PrizeWinner.Hubs
         public async Task JoinSession(string sessionId, string userName)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, sessionId);
-            var user = new User() {ConnectionId = Context.ConnectionId, UserName = userName};
+            var user = new User() { ConnectionId = Context.ConnectionId, UserName = userName };
             _sessions.Single(s => s.SessionId == sessionId)?.Users.Add(user);
 
             await Clients.Group(sessionId).SendAsync("UserJoined", user);
         }
 
+        public async Task SelectWinner()
+        {
+            var session = GetCurrentUserSession();
+            if (session?.SessionId != null)
+            {
+                var random = new Random();
+                var winner = session.Users.Where(u => !u.IsHost && session.Winners.All(w => w.ConnectionId != u.ConnectionId)).MinBy(u => Guid.NewGuid().ToString());
+                if (winner != null)
+                {
+                    winner.HasBeenWinner = true;
+                    await Clients.Group(session.SessionId).SendAsync("WinnerSelected", winner);
+                    session.Winners.Add(winner);
+                }
+            }
+        }
+
+
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
-            var session = _sessions.SingleOrDefault(s => s.Users.Any(u => u.ConnectionId == Context.ConnectionId));
+            var session = GetCurrentUserSession();
             if (session?.SessionId != null)
             {
                 var user = session.Users.Single(u => u.ConnectionId == Context.ConnectionId);
@@ -37,17 +55,26 @@ namespace PrizeWinner.Hubs
             }
             await base.OnDisconnectedAsync(exception);
         }
+
+        private Session? GetCurrentUserSession()
+        {
+            var session = _sessions.SingleOrDefault(s => s.Users.Any(u => u.ConnectionId == Context.ConnectionId));
+            return session;
+        }
     }
 
     public class Session
     {
         public string? SessionId { get; set; }
-        public List<User> Users { get; set; } = new List<User>();
+        public List<User> Users { get; set; } = new();
+        public List<User> Winners { get; set; } = new();
     }
 
     public class User
     {
         public string ConnectionId { get; set; }
         public string UserName { get; set; }
+        public bool HasBeenWinner { get; set; }
+        public bool IsHost { get; set; }
     }
 }
